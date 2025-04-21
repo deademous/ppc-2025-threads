f50fea868d91f643534ba295b3fb2c3b68bcab02
Dmitry Alekseevich <opolindmitry@gmail.com>
2025-04-21 21:27:58 +0300

    fix merge

--- tasks/tbb/opolin_d_radix_sort_batcher_merge/include/ops_tbb.hpp
+++ tasks/tbb/opolin_d_radix_sort_batcher_merge/include/ops_tbb.hpp
@@ -8,8 +8,8 @@
 
 namespace opolin_d_radix_batcher_sort_tbb {
 void BatcherMergeRadixSort(std::vector<int>& vec);
-void OddEvenMerge(std::vector<int>& vec, int left, int n, int step, int size);
-void OddEvenMergeSort(std::vector<int>& vec, int left, int n, int size);
+void CompExch(int &a, int &b);
+void BatcherSortNetwork(std::vector<int> &arr, int l, int r);
 void SortByDigit(std::vector<int>& vec);
 
 class RadixBatcherSortTaskTbb : public ppc::core::Task {
