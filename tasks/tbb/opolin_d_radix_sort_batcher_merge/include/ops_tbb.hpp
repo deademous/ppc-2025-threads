b917248d684cf9c95a9073c80d3db669ee4c4fa3
Dmitry Alekseevich <opolindmitry@gmail.com>
2025-04-21 21:32:04 +0300

    fix clang tidy2

--- tasks/tbb/opolin_d_radix_sort_batcher_merge/include/ops_tbb.hpp
+++ tasks/tbb/opolin_d_radix_sort_batcher_merge/include/ops_tbb.hpp
@@ -8,8 +8,8 @@
 
 namespace opolin_d_radix_batcher_sort_tbb {
 void BatcherMergeRadixSort(std::vector<int>& vec);
-void CompExch(int &a, int &b);
-void BatcherSortNetwork(std::vector<int> &arr, int l, int r);
+void CompExch(int& a, int& b);
+void BatcherSortNetwork(std::vector<int>& arr, int l, int r);
 void SortByDigit(std::vector<int>& vec);
 
 class RadixBatcherSortTaskTbb : public ppc::core::Task {
